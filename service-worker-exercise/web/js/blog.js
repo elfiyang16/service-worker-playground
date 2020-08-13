942cf3ac12680de1a35833585c4e78d88b625a80
Elfi Yang <elfiyang16@gmail.com>
2020-08-13 18:09:10 +0100

    feat: get basics

--- service-worker-exercise/web/js/blog.js
+++ service-worker-exercise/web/js/blog.js
@@ -1,16 +1,84 @@
-(function Blog(){
-	"use strict";
+(function Blog() {
+  "use strict";
 
-	var offlineIcon;
-	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
+  var offlineIcon;
+  var isOnline = "onLine" in navigator ? navigator.onLine : true;
+  var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
+  // let the page monitor if we are logged in since worker cannot do it
+  var usingSW = "serviceWorker" in navigator;
+  var swRegistration;
+  var svcworker;
 
-	document.addEventListener("DOMContentLoaded",ready,false);
+  document.addEventListener("DOMContentLoaded", ready, false);
 
+  if (usingSW) {
+    initServiceWorker().catch(console.error);
+  }
+  // **********************************
 
-	// **********************************
+  function ready() {
+    offlineIcon = document.getElementById("connectivity-status");
 
-	function ready() {
-		offlineIcon = document.getElementById("connectivity-status");
-	}
+    if (!isOnline) {
+      offlineIcon.classList.remove("hidden");
+    }
 
+    window.addEventListener("online", function online() {
+      offlineIcon.classList.add("hidden");
+      isOnline = true;
+      sendStatusUpdate(); //not passing in target this time
+    });
+
+    window.addEventListener("offline", function offline() {
+      offlineIcon.classList.add("hidden");
+      isOnline = false;
+      sendStatusUpdate();
+    });
+  }
+  // STEP 1. init servie worker
+  async function initServiceWorker() {
+    swRegistration = await navigator.serviceWorker.register("/sw.js", {
+      updateViaCache: "none",
+    });
+
+    var svworker =
+      swRegistration.installing ||
+      swRegistration.waiting ||
+      swRegistration.active;
+
+    navigator.serviceWorker.addEventListener(
+      "controllerchange",
+      function onController() {
+        svworker = navigator.serviceWorker.controller;
+        // proactively send status update when controller changes
+        sendStatusUpdate(svcworker);
+      }
+    );
+
+    navigator.serviceWorker.addEventListener("message", onSWMessage, false);
+
+    function onSWMessage(evt) {
+      var { data } = evt;
+      // if the worker has this requestStatusUpdate property
+      if (data.requestStatusUpdate) {
+        console.log(`Receive status update request from service worker `);
+        sendStatusUpdate(evt.ports && evt.ports[0]);
+      }
+    }
+
+    function sendStatusUpdate(target) {
+      sendSWMessage({ statusUpdate: { isOnline, isLoggedIn } }, target);
+    }
+  }
+  // Message Handling in the Client
+  // Send msg to the worker from this page
+  async function sendSWMessage(msg, target) {
+    if (target) {
+      target.postMessage(msg);
+    } else if (svcworker) {
+      svcworker.postMessage(msg);
+    } else {
+      navigator.serviceWorker.controller.postMessage(msg);
+    }
+  }
 })();
--- service-worker-exercise/web/js/sw.js
+++ service-worker-exercise/web/js/sw.js
@@ -1,3 +1,52 @@
 "use strict";
+const version = 2;
+var isOnline = true;
+var isLoggedIn = false;
 
-// TODO
+self.addEventListener("install", onInstall);
+self.addEventListener("activate", onActivate);
+self.addEventListener("message", onMessage);
+
+main().catch(console.error);
+
+async function main() {
+  await sendMessage({ requestStatusUpdate: true });
+  console.log(`service worker ${version} is starting ...`);
+}
+async function onInstall(e) {
+  console.log(`service worker ${version} is installed ...`);
+  self.skipWaiting();
+}
+
+async function sendMessage(msg) {
+  //first get all clients
+  var allClients = await clients.matchAll({ includeUncontrolled: true });
+  // give them promises
+  return Promise.all(
+    allClients.map(function sendTo(client) {
+      var chan = new MessageChannel();
+      chan.port1.onmessage = onMessage;
+      return client.postMessage(msg, [chan.port2]);
+    })
+  );
+}
+
+function onMessage({ data }) {
+  if ("statusUpdate" in data) {
+    ({ isOnline, isLoggedIn } = data.statusUpdate);
+    console.log(
+      `Service Worker (v${version}) status update... isOnline:${isOnline}, isLoggedIn:${isLoggedIn}`
+    );
+  }
+}
+
+async function onActivate(e) {
+  // tell not to shut down until handleActivation is finished
+  e.waitUntil(handleActivation());
+}
+
+async function handleActivation() {
+  //get the new service worker come into control (in case old pages still under control of old workers)
+  await client.claim();
+  console.log(`service worker ${version} is activated ...`);
+}
